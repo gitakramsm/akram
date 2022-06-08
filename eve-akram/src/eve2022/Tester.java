7c069eec0ad70587b093bafeb5a7e5e0f7ec0397
akram <akram@DESKTOP-4DA5AO5>
2022-06-08 08:59:03 +0530

    email project

---
+++ eve-akram/src/eve2022/Tester.java
@@ -0,0 +1,14 @@
+package eve2022;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.*;
+
+
+ public class Tester {
+	public static void main(String[] args) {
+		SendEmail send = new SendEmail("akram5960@gmail.com", "Allah is the Greatest", "Allahhu Akbar"); //change receiver email
+	}
+}
+
+
